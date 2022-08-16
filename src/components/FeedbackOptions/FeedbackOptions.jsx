81834ccee964a7c444653f9ec5aca87927906a6e
OlhaPetrenko <o.l.petr27@gmail.com>
2022-08-16 10:19:16 +0300

    Add defaultProps, propTypes

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -2,13 +2,21 @@ import PropTypes from 'prop-types';
 import s from './FeedbackOptions.module.css';
 
 function FeedbackOptions({ options, onLeaveFeedback }) {
-  const battonGroup = options.map(el => (
-    <button className={s.btn} type="button" onClick={() => onLeaveFeedback(el)}>
+  const battonGroup = options.map((el, index) => (
+    <button
+      key={index}
+      className={s.btn}
+      type="button"
+      onClick={() => onLeaveFeedback(el)}
+    >
       {el}
     </button>
   ));
   return <>{battonGroup}</>;
 }
+FeedbackOptions.defaultProps = {
+  options: [],
+};
 FeedbackOptions.propTypes = {
   onLeaveFeedback: PropTypes.func.isRequired,
   options: PropTypes.arrayOf(PropTypes.string),
